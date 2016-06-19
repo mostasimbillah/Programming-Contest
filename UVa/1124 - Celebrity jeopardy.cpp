23db8f20bcd1936e729bb6ff930509ad3533f8a6
mostasim <mostasim.10@gmail.com>
2016-06-20 00:47:45 +0600

    up

---
+++ UVa/1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    string str;
+
+    while(getline(cin,str))
+    {
+
+
+        cout << str<<endl;
+    }
+
+
+
+
+    return 0;
+}
+
